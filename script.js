603fc7699a647d5a04c18e6832fdc504d9213a55
Anabil Debnath <73880095+anabildebnath@users.noreply.github.com>
2024-02-19 14:36:51 +0600

    main page almost done

---
+++ script.js
@@ -0,0 +1,42 @@
+function scrollToProject() {
+  var targetProject = document.getElementById("targetProject");
+  targetProject.scrollIntoView({ behavior: "smooth" });
+}
+function scrollToAbout() {
+  var targetAbout = document.getElementById("targetAbout");
+  targetAbout.scrollIntoView({ behavior: "smooth" });
+}
+function scrollToContact() {
+  var targetContact = document.getElementById("targetContact");
+  targetContact.scrollIntoView({ behavior: "smooth" });
+}
+
+function openMail() {
+  window.open("mailto: anabildebnath16@gmail.com");
+}
+
+function scrollToTop() {
+  function smoothScroll() {
+    var currentScroll =
+      document.documentElement.scrollTop || document.body.scrollTop;
+
+    if (currentScroll > 0) {
+      window.requestAnimationFrame(smoothScroll);
+      window.scrollTo(0, currentScroll - currentScroll / 16);
+    }
+  }
+
+  
+  smoothScroll();
+}
+
+
+window.onscroll = function () {
+  var mybutton = document.getElementById("scrollToTopBtn");
+
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+};
